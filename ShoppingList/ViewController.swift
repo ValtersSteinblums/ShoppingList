f19255ad9e78bc1ce015572bed0f4563ffcb41e2
ValtersSteinblums <109601588+ValtersSteinblums@users.noreply.github.com>
2022-08-18 12:19:57 +0300

    Initial Commit

---
+++ ShoppingList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by valters.steinblums on 18/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
